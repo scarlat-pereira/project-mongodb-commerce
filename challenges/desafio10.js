ef6188f69000baa68685149ec4f69aa27243f192
Scarlat Pereira <engcivil.scarlat@gmail.com>
2023-04-20 09:05:56 -0300

    feat: create query for desafio10.js

---
+++ challenges/desafio10.js
@@ -0,0 +1,11 @@
+// use("commerce");
+db.produtos.find(
+  {
+    valoresNutricionais:
+      { $elemMatch: { tipo: "proteínas", percentual: { $gte: 30, $lte: 40 } } },
+  },
+  {
+    _id: 0,
+    nome: 1,
+  },
+);
