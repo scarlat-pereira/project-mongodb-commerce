c864914a28969f2a87f8b57dbbe27ec542790354
Scarlat Pereira <engcivil.scarlat@gmail.com>
2023-04-20 09:13:08 -0300

    feat: create query for desafio11.js

---
+++ challenges/desafio11.js
@@ -0,0 +1,10 @@
+// use("commerce");
+db.produtos.find(
+  { $and: [{ nome: { $ne: "Big Mac" } }, { nome: { $ne: "McChicken" } }] },
+  {
+    _id: 0,
+    nome: 1,
+    curtidas: 1,
+    vendidos: 1,
+  },
+);
