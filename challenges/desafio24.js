cd8d50185a2c338b0550b761c1cfb923a789846c
Scarlat Pereira <engcivil.scarlat@gmail.com>
2023-04-21 10:02:56 -0300

    feat: create query for desafio24.js

---
+++ challenges/desafio24.js
@@ -0,0 +1,7 @@
+// use("commerce");
+db.produtos.updateMany(
+  {},
+  { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } },
+);
+// use("commerce");
+db.produtos.find({}, { _id: 0, nome: 1, valoresNutricionais: 1 });
